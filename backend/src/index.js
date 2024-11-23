0eb50caccc5b562b2851397a84255fe2bef44a22
prabath1998 <prabhathudayanga1998@gmail.com>
2024-11-23 21:50:27 +0530

    implement chat option with image sending

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
-const app = express();
+// const app = express();
 
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json())
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Service listening on port ${PORT}`);
   connectDB();
 });
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -4,9 +4,9 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/signup", signup);
-router.get("/login", login);
-router.get("/logout", logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute,updateProfile);
 router.get("/check", protectRoute, checkAuth);
