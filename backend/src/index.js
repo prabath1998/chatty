2c19d8153a4c75ac298b30cc60fec4e5dba773bc
prabath1998 <prabhathudayanga1998@gmail.com>
2024-11-23 16:10:00 +0530

    implement auth endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
+dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json())
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-  console.log("Service listening on port 5001");
+app.listen(PORT, () => {
+  console.log(`Service listening on port ${PORT}`);
+  connectDB();
 });
